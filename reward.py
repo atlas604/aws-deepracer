db723db6e3d7c67d7a3023aa9bc0e1e934f0af32
atlas604 <19612461+atlas604@users.noreply.github.com>
2019-08-07 23:35:14 -0400

    Update reward.py

--- reward.py
+++ reward.py
@@ -11,20 +11,13 @@ def reward_function(params):
     progress = params['progress']
     steering = abs(params['steering_angle'])
 
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    # Give a very low reward by default
+    reward = 1e-3
 
-    # Give higher reward if the agent is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
 
     # Steering penality threshold, change the number based on your action space setting
     ABS_STEERING_THRESHOLD = 15
