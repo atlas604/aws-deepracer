814eec474b0628809ddc7cfcc166ac01ae451d99
atlas604 <19612461+atlas604@users.noreply.github.com>
2019-08-20 10:37:32 -0400

    Update reward.py

--- custom_files/reward.py
+++ custom_files/reward.py
@@ -2,12 +2,13 @@ def reward_function(params):
     
     all_wheels_on_track = params['all_wheels_on_track']
     progress = params['progress']
-    steps = params["steps"]
+    steps = params['steps']
+    speed = params['speed']
     
     if all_wheels_on_track and steps > 0:
-        reward = (progress / steps) * 100
+        reward = progress / steps * (speed / 9)
     
     else:
-        reward = -1
+        reward = 1e-3
     
     return float(reward)
